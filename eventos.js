0d32ccc289800643fb8001d070fd43ff97686183
Apeiros <angel14rinconjimenez@gmail.com>
2026-01-20 09:56:13 -0600

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,33 @@
+const EVENTOS = [
+  {
+    id: "batalla-del-barajaverso",
+    title: "Guerra De Los Juegos De Mesa: Barajaverso",
+    description: "Te has preguntado que universo de Barajas es el mejor, en este universo ambos entran en batalla, pero solo uno ganará.",
+    image: "images/batalla-del-barajaverso.jpg"
+  },
+  {
+    id: "batalla-del-cheesverso",
+    title: "Guerra de Los Juegos de Mesa: Cheesverse",
+    description: "Te has preguntado que universo de Ajedrez es el mejor, en este universo ambos entran en batalla, pero solo uno ganará.",
+    image: "images/batalla-del-cheesverse.jpg"
+  },
+  {
+    id: "La-guerra-de-los-juegos-de-mesa_Chess-vs-Domino",
+    title: "Chess vs. Domino",
+    description: "Te has preguntado que universo de Ajedrez es el mejor, en este universo ambos entran en batalla, pero solo uno ganará.",
+    image: "images/batalla-del-cheesverse.jpg"
+  },
+  {
+    id: "La-guerra-de-los-juegos-de-mesa_Barajas-vs-Jenga",
+    title: "Chess vs. Domino",
+    description: "Te has preguntado que universo de Ajedrez es el mejor, en este universo ambos entran en batalla, pero solo uno ganará.",
+    image: "images/batalla-del-cheesverse.jpg"
+  },
+  {
+    id: "La-guerra-de-los-juegos-de-mesa_Chess-vs-Baraja",
+    title: "Chess vs. Domino",
+    description: "Te has preguntado que universo de Ajedrez es el mejor, en este universo ambos entran en batalla, pero solo uno ganará.",
+    image: "images/batalla-del-cheesverse.jpg"
+  }
+
+  ]
